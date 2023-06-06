dcadd1a9908f2485615c705a98989a737f110944
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-06 10:20:30 +0530

    Division Of Two Input Numbers

---
+++ Division.java
@@ -0,0 +1,22 @@
+// Get Division Of Two Numbers
+
+import java.util.*;
+
+class Division {
+    public static void main(String args[]) {
+        System.out.println("Obtaining the value of the division of two numbers \n");
+
+        Scanner input=new Scanner(System.in);
+
+        int x,y,z;
+        System.out.print("Input Your 1st Number : ");
+        x=input.nextInt();
+
+        System.out.print("Input Your 2nd Number : ");
+        y=input.nextInt();
+
+        z=x/y;
+        System.out.print("Final Answer Is : " +z);
+
+    }
+}

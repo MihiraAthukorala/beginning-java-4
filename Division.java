aca7e8cb3698481629fc56bf4dfbc18299b39939
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-11 22:17:02 +0530

    Divission 11.6.2023

--- Division.java
+++ Division.java
@@ -1,14 +1,40 @@
 // Get Division Of Two Numbers
 
+// import java.util.*;
+
+// class Division {
+//     public static void main(String args[]) {
+//         System.out.println("Obtaining the value of the division of two numbers \n");
+
+//         Scanner input=new Scanner(System.in);
+
+//         int x,y,z;
+//         System.out.print("Input Your 1st Number : ");
+//         x=input.nextInt();
+
+//         System.out.print("Input Your 2nd Number : ");
+//         y=input.nextInt();
+
+//         z=x/y;
+//         System.out.print("Final Answer Is : " +z);
+
+//     }
+// }
+
+// Get Division Of Two Numbers
+
 import java.util.*;
 
 class Division {
+
     public static void main(String args[]) {
-        System.out.println("Obtaining the value of the division of two numbers \n");
 
-        Scanner input=new Scanner(System.in);
+        System.out.println("Obtaining The Value Of The Division Of Two Numbers \n");
 
-        int x,y,z;
+        Scanner input = new Scanner(System.in);
+
+        int x, y, z;
+
         System.out.print("Input Your 1st Number : ");
         x=input.nextInt();
 
@@ -16,7 +42,8 @@ class Division {
         y=input.nextInt();
 
         z=x/y;
-        System.out.print("Final Answer Is : " +z);
+
+        System.out.println("Your Answer Is : "+z);
 
     }
 }
